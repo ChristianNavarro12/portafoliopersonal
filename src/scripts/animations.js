da92e342a8e55c97f860c1d49ad8e5383749eaf2
ChristianNavarro12 <christiannavarro12003@gmail.com>
2026-08-24 13:21:18 -0600

    agregando archivos

---
+++ src/scripts/animations.js
@@ -0,0 +1,7 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+      AOS.init({
+        duration: 800, // Duración de la animación en milisegundos
+        once: true,    // Si la animación debe ocurrir solo una vez
+      });
